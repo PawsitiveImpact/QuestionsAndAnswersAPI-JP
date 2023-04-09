b8160c67887df72fb25472434feb41e92d76db74
Jenessa Peterson <jenessa.peterson@gmail.com>
2023-04-09 10:14:03 -0700

    Console logging from models for all endpoints.

--- controllers.js
+++ controllers.js
@@ -8,101 +8,101 @@ module.exports = {
       } else {
         res.json(data);
       }
-    })
+    });
   },
 
   getAllQuestions: (req, res) => {
     console.log("got to getAllQuestions with this product id: ", req.params.product_id);
-    // models.getAllQuestions((err, data) => {
-    //   if(err){
-    //     console.log(err);
-    //   } else {
-    //     res.json(data);
-    //   }
-    // })
+    models.getAllQuestions(req.params.product_id, (err, data) => {
+      if(err){
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    });
   },
 
   getAllAnswers: (req, res) => {
     console.log("got to getAllAnswers with this question_id: ", req.params.question_id);
 
-    // models.getAllQuestions((err, data) => {
-    //   if(err){
-    //     console.log(err);
-    //   } else {
-    //     res.json(data);
-    //   }
-    // })
+    models.getAllAnswers(req.params.question_id, (err, data) => {
+      if(err){
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    });
   },
 
   postQuestion: (req, res) => {
     console.log("got to postQuestion with this req.body: ", req.body);
 
-    // models.getAllQuestions((err, data) => {
-    //   if(err){
-    //     console.log(err);
-    //   } else {
-    //     res.json(data);
-    //   }
-    // })
+    models.postQuestion(req.body, (err, data) => {
+      if(err){
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    });
   },
 
   postAnswer:(req, res) => {
     console.log("got to postAnswer with this req.body: ", req.body, " and this question_id: ", req.params.question_id);
 
-    // models.getAllQuestions((err, data) => {
-    //   if(err){
-    //     console.log(err);
-    //   } else {
-    //     res.json(data);
-    //   }
-    // })
+    models.postAnswer(req.body, req.params.question_id, (err, data) => {
+      if(err){
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    })
   },
 
   putHelpfulQ:(req, res) => {
     console.log("got to putHelpfulQ with this question_id: ", req.params.question_id);
 
-    // models.getAllQuestions((err, data) => {
-    //   if(err){
-    //     console.log(err);
-    //   } else {
-    //     res.json(data);
-    //   }
-    // })
+    models.putHelpfulQ(req.params.question_id, (err, data) => {
+      if(err){
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    })
   },
 
   putReportQ: (req, res) => {
     console.log("got to putReportQ with this question_id: ", req.params.question_id);
 
-    // models.getAllQuestions((err, data) => {
-    //   if(err){
-    //     console.log(err);
-    //   } else {
-    //     res.json(data);
-    //   }
-    // })
+    models.putReportQ(req.params.question_id, (err, data) => {
+      if(err){
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    })
   },
 
   putHelpfulA:(req, res) => {
     console.log("got to putHelpfulA with this answer_id: ", req.params.answer_id);
 
-    // models.getAllQuestions((err, data) => {
-    //   if(err){
-    //     console.log(err);
-    //   } else {
-    //     res.json(data);
-    //   }
-    // })
+    models.putHelpfulA(req.params.answer_id, (err, data) => {
+      if(err){
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    })
   },
 
   putReportA: (req, res) => {
     console.log("got to putReportA with this answer_id: ", req.params.answer_id);
 
-    // models.getAllQuestions((err, data) => {
-    //   if(err){
-    //     console.log(err);
-    //   } else {
-    //     res.json(data);
-    //   }
-    // })
+    models.putReportA(req.params.answer_id, (err, data) => {
+      if(err){
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    })
   }
 };
--- models.js
+++ models.js
@@ -10,5 +10,100 @@ module.exports = {
         callback(null, data);
       }
     });
+  },
+
+  getAllQuestions: (product_id, callback) => {
+    console.log("got to models.getAllQuestions with this: ", product_id);
+    // var queryStr = 'SELECT * FROM questions LIMIT 2';
+    // db.query(queryStr, (err, data) => {
+    //   if (err) {
+    //     callback(err);
+    //   } else {
+    //     callback(null, data);
+    //   }
+    // });
+  },
+
+  getAllAnswers: (question_id, callback) => {
+    console.log("got to models.getAllAnswers with this: ", question_id);
+    // var queryStr = 'SELECT * FROM questions LIMIT 2';
+    // db.query(queryStr, (err, data) => {
+    //   if (err) {
+    //     callback(err);
+    //   } else {
+    //     callback(null, data);
+    //   }
+    // });
+  },
+
+  postQuestion: (questionObj, callback) => {
+    console.log("got to models.postQuestion with this questionObj: ", questionObj);
+    // var queryStr = 'SELECT * FROM questions LIMIT 2';
+    // db.query(queryStr, (err, data) => {
+    //   if (err) {
+    //     callback(err);
+    //   } else {
+    //     callback(null, data);
+    //   }
+    // });
+  },
+
+  postAnswer: (answerObj, question_id, callback) => {
+    console.log("got to models.postQuestion with this answerObj: ", answerObj, " and this question_id: ", question_id);
+    // var queryStr = 'SELECT * FROM questions LIMIT 2';
+    // db.query(queryStr, (err, data) => {
+    //   if (err) {
+    //     callback(err);
+    //   } else {
+    //     callback(null, data);
+    //   }
+    // });
+  },
+
+  putHelpfulQ:(question_id, callback) => {
+    console.log("got to models.putHelpfulQ with this question_id: ", question_id);
+    // var queryStr = 'SELECT * FROM questions LIMIT 2';
+    // db.query(queryStr, (err, data) => {
+    //   if (err) {
+    //     callback(err);
+    //   } else {
+    //     callback(null, data);
+    //   }
+    // });
+  },
+
+  putReportQ:(question_id, callback) => {
+    console.log("got to models.putReportQ with this question_id: ", question_id);
+    // var queryStr = 'SELECT * FROM questions LIMIT 2';
+    // db.query(queryStr, (err, data) => {
+    //   if (err) {
+    //     callback(err);
+    //   } else {
+    //     callback(null, data);
+    //   }
+    // });
+  },
+
+  putHelpfulA:(answer_id, callback) => {
+    console.log("got to models.putHelpfulA with this answer_id: ", answer_id);
+    // var queryStr = 'SELECT * FROM questions LIMIT 2';
+    // db.query(queryStr, (err, data) => {
+    //   if (err) {
+    //     callback(err);
+    //   } else {
+    //     callback(null, data);
+    //   }
+    // });
+  },
+  putReportA:(answer_id, callback) => {
+    console.log("got to models.putReportA with this answer_id: ", answer_id);
+    // var queryStr = 'SELECT * FROM questions LIMIT 2';
+    // db.query(queryStr, (err, data) => {
+    //   if (err) {
+    //     callback(err);
+    //   } else {
+    //     callback(null, data);
+    //   }
+    // });
   }
-}
+};
